77cb8f04aff20be1cdf5a4a7d21bb5df5c646122
irvan.din <irfannudin839@gmail.com>
2024-04-18 15:32:32 +0700

    tt

--- app/src/main/java/com/example/proyekstarling/fraglayananadmin.kt
+++ app/src/main/java/com/example/proyekstarling/fraglayananadmin.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 
 class fraglayananadmin : Fragment () {
     lateinit var thisParent: dashboardadmin
-
+//
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
