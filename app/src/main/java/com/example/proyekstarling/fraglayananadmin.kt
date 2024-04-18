b1798347bd65634193b592110edbdeea0e82aaa1
irvan.din <irfannudin839@gmail.com>
2024-04-18 15:19:41 +0700

    tt

--- app/src/main/java/com/example/proyekstarling/fraglayananadmin.kt
+++ app/src/main/java/com/example/proyekstarling/fraglayananadmin.kt
@@ -1,13 +1,16 @@
 package com.example.proyekstarling
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class fraglayananadmin : Fragment () {
-    lateinit var thisParent : dashboardadmin
+    lateinit var thisParent: dashboardadmin
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +18,20 @@ class fraglayananadmin : Fragment () {
         savedInstanceState: Bundle?
     ): View? {
         thisParent = activity as dashboardadmin
-        return inflater.inflate(R.layout.fraglayananadmin, container, false)
+        val view = inflater.inflate(R.layout.fraglayananadmin, container, false)
+        val button3: Button = view.findViewById(R.id.button3)
+
+        button3.setOnClickListener {
+            val url = "https://wa.me/6282143231811"
+            val intentwa = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intentwa)
+        }
+        return view
+    }
+
+    fun onButtonClicked(view: View) {
+        val url = "https://wa.me/6282143231811"
+        val intentwa = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intentwa)
     }
 }
